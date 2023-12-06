f54834fffab086f2149c44a6dcf38a4387689401
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2023-12-06 20:13:41 -0300

    LoggerManager and TelegramManager

---
+++ SL/Services/LoggerManager.cs
@@ -0,0 +1,104 @@
+﻿using SL.DAL.Tools;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SL.Services
+{
+    
+		public class LoggerManager
+		{
+			private readonly static LoggerManager _instance = new LoggerManager();
+
+			public static LoggerManager Current
+			{
+				get
+				{
+					return _instance;
+				}
+			}
+
+			private LoggerManager()
+			{
+				//Implent here the initialization of your singleton
+			}
+
+        public async Task Write(string mensaje, EventLevel evento)
+        {
+            DateTime fecha = DateTime.Now;
+
+            switch (evento)
+            {
+               
+                case EventLevel.Informational:
+               
+                    
+                    try
+                    {
+                        SqlParameter[] parameters = new SqlParameter[]
+                        {
+                    new SqlParameter("@fecha",fecha),
+                    new SqlParameter("@evento",evento),
+                    new SqlParameter("@mensaje",mensaje),
+                 
+                        };
+
+                        int rowsAffected = SqlHelper.ExecuteNonQuery("INSERT INTO [dbo].[Bitacora] (fecha, evento, mensaje) VALUES (@fecha, @evento, @mensaje)", System.Data.CommandType.Text, parameters);
+
+                       
+                    }
+                    catch (SqlException ex)
+                    {
+                        throw new ApplicationException("Ocurrió un error interno del servidor al registrar la bitacora. Por favor, contacte al soporte técnico.", ex);
+                    }
+                    break;
+
+
+                case EventLevel.Critical:
+
+                    
+                    try
+                    {
+                        SqlParameter[] parameters = new SqlParameter[]
+                        {
+                    new SqlParameter("@fecha",fecha),
+                    new SqlParameter("@evento",evento),
+                    new SqlParameter("@mensaje",mensaje),
+
+                        };
+
+                        int rowsAffected = SqlHelper.ExecuteNonQuery("INSERT INTO [dbo].[Bitacora] (fecha, evento, mensaje) VALUES (@fecha, @evento, @mensaje)", System.Data.CommandType.Text, parameters);
+
+
+                        
+
+                    }
+                    catch (SqlException ex)
+                    {
+                        throw new ApplicationException("Ocurrió un error interno del servidor al registrar la bitacora. Por favor, contacte al soporte técnico.", ex);
+                    }
+
+
+                    string messageText = $"Tenes un evento critico del tipo {evento}";
+
+                    
+                    await TelegramManager.Current.SendMessage(messageText);
+
+
+
+                    break;
+
+            }
+        }
+
+    }
+
+	
+}
---
+++ SL/Services/TelegramManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SL.Services
+{
+
+    internal sealed class TelegramManager
+    {
+        private readonly static TelegramManager _instance = new TelegramManager();
+        private readonly string botToken;
+        private readonly string chatId;
+
+        public static TelegramManager Current
+        {
+            get { return _instance; }
+        }
+
+        private TelegramManager()
+        {
+            
+            botToken = ConfigurationManager.AppSettings["Token"];
+            chatId = ConfigurationManager.AppSettings["ChatId"];
+        }
+
+        public async Task SendMessage(string messageText)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string apiUrl = $"https://api.telegram.org/bot{botToken}/sendMessage";
+
+                var content = new StringContent($"chat_id={chatId}&text={Uri.EscapeDataString(messageText)}", Encoding.UTF8, "application/x-www-form-urlencoded");
+
+                HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+                response.EnsureSuccessStatusCode(); // Lanza una excepción si el código de estado no es de éxito.
+
+                // Puedes agregar lógica adicional después de asegurar el éxito, si es necesario.
+            }
+        }
+    }
+
+}
